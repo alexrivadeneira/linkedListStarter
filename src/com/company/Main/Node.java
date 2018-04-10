077fd150e4680e1f3e142afe848b7ea017125ee7
alexrivadeneira <alexrivs@gmail.com>
2018-04-10 11:49:52 -0700

    remove node methods

--- src/com/company/Main/Node.java
+++ src/com/company/Main/Node.java
@@ -2,28 +2,5 @@ package com.company.Main;
 
 public class Node {
     // Node in the Singly Linked List.  Nodes store ints. Default data value is 0.
-    private int data;
-    private Node next;
-
-    public int getData() {
-        return data;
-    }
-
-    public void setData(int data) {
-        this.data = data;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    // constructor, takes int
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
+   
 }
